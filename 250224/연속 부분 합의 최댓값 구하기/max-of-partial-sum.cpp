8bded54c5c362ec00272cba51e17c0dec8c5cdcc
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-02-24 22:59:38 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: 1004ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 250224/연속 부분 합의 최댓값 구하기/max-of-partial-sum.cpp
+++ 250224/연속 부분 합의 최댓값 구하기/max-of-partial-sum.cpp
@@ -20,15 +20,24 @@ int main() {
 
         else {
             int num = 0;
-            for(int i = cnt; i>=0; i--) {
+            int sub_cnt = 0;
+            for(int i = cnt; i>=sub_cnt; i--) {
                 num = num + arr[i];
                 if(num > max) {
                     max = num;
+                }
+                if(num <= 0) {
+                    sub_cnt = i;
                 }
             }
         }
     }
     cout << max;
 
+
+    //시간을 줄이려면 두번째 포문이 0으로 가면 안된다.
+    //최소 변수를 설정해서, 그 아래로 가도록 하면 안된다.
+    //더 작아지는 경우는 결국 음수가 되거나 0인 경우
+    //그렇다면 음수가 될 때의 cnt를 기억하면 되지 않을까?
     return 0;
 }
