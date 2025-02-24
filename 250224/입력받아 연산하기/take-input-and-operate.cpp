fd3bc11db0c763c859a1104a4031bcd331008c16
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-02-24 14:08:53 +0900

    Title: 입력받아 연산하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250224/입력받아 연산하기/take-input-and-operate.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 0;
+    int b = 0;
+    cin >> a >> b;
+
+    cout << a+87 << endl;
+    cout << b%10 << endl;
+    return 0;
+}
