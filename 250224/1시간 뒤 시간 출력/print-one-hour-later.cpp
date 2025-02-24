e7f6ce456472c75ff0d8b14a1efa7ac739563755
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-02-24 13:36:21 +0900

    Title: 1시간 뒤 시간 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250224/1시간 뒤 시간 출력/print-one-hour-later.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int hour = 0;
+    int min = 0;
+    char middle = NULL;
+    cin >> hour >> middle >> min;
+
+    cout << hour + 1 << middle << min << endl;
+    return 0;
+}
