e1979d034cc3e9a9258db8ea74e9984d8a4939ba
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-02-24 10:59:37 +0900

    Title: 변수 값 복사하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250224/변수 값 복사하기 2/copying-variable-values-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 5;
+    int b = 6;
+    int c = 7;
+
+    a = c;
+    b = c;
+
+    cout << a << " " << b << " " << c << endl;
+    return 0;
+}
