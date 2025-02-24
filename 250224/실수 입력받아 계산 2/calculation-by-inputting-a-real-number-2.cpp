ccd80a4ff9740a4d6817e6c7ee00e08c5a9c856c
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-02-24 11:23:05 +0900

    Title: 실수 입력받아 계산 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250224/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << fixed;
+    cout.precision(2);
+
+    double a = 0;
+    cin >> a;
+
+    cout << a + 1.5 << endl;
+    return 0;
+}
