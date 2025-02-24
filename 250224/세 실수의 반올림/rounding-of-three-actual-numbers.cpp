c46557956dbd06127031b68f7cdafa7bed919355
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-02-24 13:19:06 +0900

    Title: 세 실수의 반올림, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250224/세 실수의 반올림/rounding-of-three-actual-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << fixed;
+    cout.precision(3);
+    
+    double a = 0;
+    double b = 0;
+    double c = 0;
+    cin >> a >> b >> c;
+
+    cout << a << endl << b << endl << c << endl;
+    return 0;
+}
