ea203a3414a7e0e441e4b5dd270ebafb234c6afe
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-02-24 10:22:27 +0900

    Title: 소수점 반올림하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250224/소수점 반올림하기/rounding-decimal-points.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << fixed;
+    cout.precision(1);
+
+    double a = 25.352;
+    
+    cout << a << endl;
+    return 0;
+}
