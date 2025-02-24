ab57bcbd7137e1d34432361b2f7563b7d9f975c7
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-02-24 10:46:24 +0900

    Title: 변수 값 교체하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250224/변수 값 교체하기 2/replacing-variable-values-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 5;
+    int b = 3;
+    
+    a = b;
+
+    cout << a << endl;
+    cout << b << endl;
+    return 0;
+}
