2951f10c43a49222bea24897fff41bddaff3a91f
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-02-24 13:41:20 +0900

    Title: 입력받아 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250224/입력받아 출력 2/input-and-output-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int first = 0;
+    int last = 0;
+    char middle = NULL;
+
+    cin >> first >> middle >> last;
+
+    cout << first << last;
+    return 0;
+}
