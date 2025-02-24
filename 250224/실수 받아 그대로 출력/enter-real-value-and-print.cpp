06d68a222a6cdfea3c59d506e14b87e31b15efb0
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-02-24 11:14:54 +0900

    Title: 실수 받아 그대로 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250224/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << fixed;
+    cout.precision(2);
+
+    double a = 0;
+    cin >> a;
+
+    cout << a << endl;
+    return 0;
+}
