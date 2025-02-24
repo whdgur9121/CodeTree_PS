c712a401de7d8792cf0bd3382033e3681b6b2b29
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-02-24 11:07:21 +0900

    Title: 점수 입출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250224/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 0;
+    cin >> a;
+
+    cout << "Your score is " << a << " point." << endl;
+    return 0;
+}
