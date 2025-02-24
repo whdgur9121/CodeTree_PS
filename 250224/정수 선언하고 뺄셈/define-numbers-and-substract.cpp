b3ad68241ab40fe5ce532e327c77f6619aa08aa3
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-02-24 10:12:00 +0900

    Title: 정수 선언하고 뺄셈, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250224/정수 선언하고 뺄셈/define-numbers-and-substract.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 97;
+    int b = 13;
+    cout << a << " - " << b << " = " << a-b;
+    return 0;
+}
