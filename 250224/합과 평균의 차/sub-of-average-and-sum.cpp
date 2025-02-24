39b6cd0a50ac1727b215f7ac8c49443045a269c5
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-02-24 14:25:39 +0900

    Title: 합과 평균의 차, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250224/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    cin >> a >> b >> c;
+
+    int sum = a + b + c;
+    int mean = sum / 3;
+    cout << sum << endl << mean << endl << sum - mean << endl;
+    return 0;
+}
