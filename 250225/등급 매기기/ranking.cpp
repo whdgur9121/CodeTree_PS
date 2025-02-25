b679c62825fbffe4ca9949b7cd8e1334df5b583b
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-02-25 15:31:26 +0900

    Title: 등급 매기기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250225/등급 매기기/ranking.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 0;
+    cin >> a;
+
+    if(a >= 90) {
+        cout << "A" << endl;
+    }
+    else if(a >= 80) {
+        cout << "B" << endl;
+    }
+    else if(a >= 70) {
+        cout << "C" << endl;
+    }
+    else if(a >= 60) {
+        cout << "D" << endl;
+    }
+    else {
+        cout << "F" << endl;
+    }
+    return 0;
+}
