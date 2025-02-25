038a852141861d5c92b1e55c1b443802fe8387d1
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-02-25 14:36:00 +0900

    Title: 정수의 조건 여부, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250225/정수의 조건 여부/numbers-condition.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 0;
+    cin >> a;
+
+    if(a >= 113) {
+        cout << 1 << endl;
+    }
+    else {
+        cout << 0 << endl;
+    }
+    return 0;
+}
