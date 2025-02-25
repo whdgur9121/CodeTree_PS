7b0793968999692088051738e0b2638030b1594d
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-02-25 15:21:09 +0900

    Title: 물의 상태, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250225/물의 상태/state-of-water.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int tem = 0;
+    cin >> tem;
+
+    if(tem < 0) {
+        cout << "ice" << endl;
+    }
+    else if(tem > 100) {
+        cout << "vapor" << endl;
+    }
+    else {
+        cout << "water" << endl;
+    }
+    return 0;
+}
