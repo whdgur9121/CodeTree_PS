c9d62d330e41f96f4428e89d295aaf8e3a21e2ca
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-02-25 15:18:32 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250225/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int exam = 0;
+    cin >> exam;
+
+    if(exam == 100) {
+        cout << "pass" << endl;
+    }
+    else {
+        cout << "failure" << endl;
+    }
+    return 0;
+}
