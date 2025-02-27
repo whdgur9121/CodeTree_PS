4671d9b4f8f78813a4f0cca7bc839ec8afdc2e8b
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-02-27 10:12:54 +0900

    Title: DateTime to DateTime, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250227/DateTime to DateTime/datetime-to-datetime.cpp
+++ 250227/DateTime to DateTime/datetime-to-datetime.cpp
@@ -12,21 +12,25 @@ int main() {
     int hour = b;
     int min = c;
 
-    if(date < 11 && hour < 11 && min < 11) {
-
-    }
-
     int result = (a * 24 * 60) + (b * 60) + c;
     int start = (11 * 24 * 60) + (11 * 60) + 11;
 
     int cnt = 0;
+
+    if(result <= start) {
+        cout << -1 << endl;
+        return 0;
+    }
 
     while(start != result) {
         start++;
         cnt++;
     }
 
+
     cout << cnt << endl;
+    
+    
 
     return 0;
 }
