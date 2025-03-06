6f3b8d0a2bc02b6ea7a8c998939c43ea3427b0d5
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-03-06 11:05:52 +0900

    Title: 격자 위의 편안한 상태, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250306/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+int N, M;
+int r[10000], c[10000];
+//편안한 상태면 1
+//불편한 상태면 0
+
+int main() {
+    cin >> N >> M;
+    //N*N 격자, M개 칠하기
+
+    for (int i = 0; i < M; i++) {
+        cin >> r[i] >> c[i];
+    }
+
+    // Please write your code here.
+    int arr[N+1][N+1];
+    for(int i = 0; i<=N; i++) {
+        for(int j = 0; j<=N; j++) {
+            arr[i][j] = 0;
+        }
+    }
+
+    int dx[4] = {1, 0, -1, 0};
+    int dy[4] = {0, 1, 0, -1};
+
+    for(int i = 0; i < M; i++) {
+        int x = r[i];
+        int y = c[i];
+
+        arr[x][y] = 1;
+        
+        int cnt = 0;
+        for(int j = 0; j < 4; j++) {
+            x = x + dx[j];
+            y = y + dy[j];
+            if(arr[x][y] == 1) {
+                cnt ++;
+            }
+            x = x - dx[j];
+            y = y - dy[j];
+        }
+
+        if(cnt >= 3) {
+            cout << 1 << endl;
+        }
+        else {
+            cout << 0 << endl;
+        }
+    }
+
+    return 0;
+}
