fd16b47e4066b26ebd2743b8a18465791e73c99f
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-03-07 21:13:44 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 250307/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.cpp
+++ 250307/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.cpp
@@ -24,23 +24,8 @@ int main() {
 
     for(int i = 0; i < N*N; i++) {
         cout << arr[x][y] << " ";
-
-        int dir_num = 0;
-        int cnt = 0;
-        for(int j = 0; j<3; j++) {
-            if(arr[x][y] < arr[x + dx[dir_num]][y + dy[dir_num]]) {
-                x = x+dx[dir_num];
-                y = y+dy[dir_num];
-                j = 3;
-            }
-            dir_num = (dir_num++) % 4;
-            cnt++;
-        }
-        if(cnt == 4) {
-            break;
-        }
         
-        if(arr[x][y] < arr[x + dx[0]][y + dy[0]] && (x + dx[0]) > 0) {
+        if(arr[x][y] < arr[x + dx[0]][y + dy[0]] && (x + dx[0]) >= 0) {
             x = x+dx[0];
             y = y+dy[0];
         }
@@ -48,7 +33,7 @@ int main() {
             x = x+dx[1];
             y = y+dy[1];
         }
-        else if(arr[x][y] < arr[x + dx[2]][y + dy[2]] && (y + dy[2]) > 0) {
+        else if(arr[x][y] < arr[x + dx[2]][y + dy[2]] && (y + dy[2]) >= 0) {
             x = x+dx[2];
             y = y+dy[2];
         }
@@ -66,25 +51,3 @@ int main() {
 
     return 0;
 }
-
-/*
-if(arr[x][y] < arr[x + dx[0]][y + dy[0]] && (x + dx[0]) > 0) {
-            x = x+dx[0];
-            y = y+dy[0];
-        }
-        else if(arr[x][y] < arr[x + dx[1]][y + dy[1]] && (x + dx[1]) < N) {
-            x = x+dx[1];
-            y = y+dy[1];
-        }
-        else if(arr[x][y] < arr[x + dx[2]][y + dy[2]] && (y + dy[2]) > 0) {
-            x = x+dx[2];
-            y = y+dy[2];
-        }
-        else if(arr[x][y] < arr[x + dx[3]][y + dy[3]]&& (y + dy[3]) < N) {
-            x = x+dx[3];
-            y = y+dy[3];
-        }
-        else {
-            break;
-        }
-*/
