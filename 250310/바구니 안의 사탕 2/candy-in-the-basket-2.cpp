cf0ba30d249aeb6ec0b4c29f4ff8de8ac618c360
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-03-10 11:12:48 +0900

    Title: 바구니 안의 사탕 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250310/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+//1차원 직선, N개의 바구니
+//바구니 안에는 사탕
+// 중심점 C를 잡아 [c -k, c+k 구간에 있는 사탕 수 최대]
+
+//input
+//N과 K
+//N개의 줄에 걸쳐 바구니 좌표, 사탕 개수
+int main() {
+    int N, K;
+    cin >> N >> K;
+
+    int arr[101];
+    for(int i = 0; i<101; i++) {
+        arr[i] = 0;
+    }
+
+    int candy = 0;
+    int index = 0;
+
+    for (int i = 0; i < N; i++) {
+        cin >> candy >> index;
+        arr[index] = candy;
+    }
+    /*
+    for(int i = 0; i<101; i++) {
+        cout << arr[i] << " " ;
+    }
+    */
+
+    int max = 0;
+    for(int i = K; i <= N; i++) {
+        int cnt = 0;
+        //cout << "Operation" << endl;
+        for(int j = i - K; j <= i+K; j++) {
+            cnt = cnt + arr[j];
+            //cout << arr[j] << " ";
+        }
+        //cout << endl;
+        if(cnt > max) {
+            max = cnt;
+        }
+    }
+
+    cout << max;
+
+    // Please write your code here.
+
+    return 0;
+}
