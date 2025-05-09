89f6982c0ff067098dc2cb14e3e4feef388e270c
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-05-09 19:55:39 +0900

    Title: 정수 사각형 최대 합, Time: 10ms, Memory: 0MB, Status: Passed - Codetree

--- 250509/정수 사각형 최대 합/maximum-sum-path-in-square.cpp
+++ 250509/정수 사각형 최대 합/maximum-sum-path-in-square.cpp
@@ -26,27 +26,30 @@ int main() {
 
     // Please write your code here.
 
-    int dp[100][100];
+    long long dp[100][100];
     for(int i = 0; i<100; i++) {
         for(int k = 0; k<100; k++) {
             dp[i][k] = 0;
         }
     }
+
     dp[0][0] = grid[0][0];
+    for(int i = 1; i<n; i++) {
+        dp[0][i] = dp[0][i-1] + grid[0][i];
+    }
+    for(int i = 1; i<n; i++) {
+        dp[i][0] = dp[i-1][0] + grid[i][0];
+    }
 
     //범위를 벗어나면 0을 넣어주면 될듯
     for(int i = 1; i<n; i++) {
-        for(int k = 0; k<n; k++) {
-            if(k-1 < 0) {
-                dp[i][k] = grid[i][k] + max(dp[i][k-1], dp[i-1][k]);
-            }
-            else {
-                dp[i][k] = grid[i][k] + max(dp[i][k-1], dp[i-1][k]);
-            }
+        for(int k = 1; k<n; k++) {
+            dp[i][k] = grid[i][k] + max(dp[i][k-1], dp[i-1][k]);
+            
         }
     }
 
-    int Max = 0;
+    long long Max = 0;
     for(int i = 0; i<n; i++) {
         if(Max < dp[n-1][i]) {
             Max = dp[n-1][i];
