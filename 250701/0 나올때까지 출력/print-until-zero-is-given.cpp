481ff0cf068b773f4375598547d094e443476f87
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-07-01 15:14:51 +0900

    Title: 0 나올때까지 출력, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250701/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    while(1) {
+        int a;
+        cin >> a;
+        if(a == 0) {
+            break;
+        }
+        else {
+            cout << a << endl;
+        }
+    }
+    return 0;
+}
