ecf9f9a67c458a4c81cc42a7f93cb4bd9523d25f
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-07-01 14:54:23 +0900

    Title: 조건에 부합하는 수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250701/조건에 부합하는 수/number-that-meets-the-condition.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a;
+    cin >> a;
+
+    for(int i = 1; i<=a; i++) {
+        if(i%2 == 0 && i%4 != 0) {
+            continue;
+        }
+        if((i/8)%2 == 0) {
+            continue;
+        }
+        if((i%7) < 4) {
+            continue;
+        }
+        cout << i << " ";
+    }
+    return 0;
+} 
