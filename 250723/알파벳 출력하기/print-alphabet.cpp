059a651a04ddae467270ea0b40adb87016c38778
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-07-23 22:00:31 +0900

    Title: 알파벳 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250723/알파벳 출력하기/print-alphabet.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    char cnt = 'A';
+
+    for(int i = 0; i<n; i++) {
+        for(int k = 0; k<=i; k++) {
+            cout << cnt;
+            cnt++;
+            if(cnt == '[') {
+                cnt = 'A';
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
