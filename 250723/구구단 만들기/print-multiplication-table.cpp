10f91ecd43b42997069bef1611056073edc1a2a3
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-07-23 14:54:28 +0900

    Title: 구구단 만들기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250723/구구단 만들기/print-multiplication-table.cpp
+++ 250723/구구단 만들기/print-multiplication-table.cpp
@@ -7,7 +7,7 @@ int main() {
     cin >> a >> b;
 
     for(int i = 1; i<=9; i++) {
-        for(int k = b; k > 0; k = k-2) {
+        for(int k = b; k >= a; k = k-2) {
             cout << k << " * " << i << " = " << k*i;
             if(k != 2) {
                 cout << " / ";
