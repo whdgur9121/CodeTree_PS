0dcf8e2cea1369157e468867b7f3a5815d724322
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-07-23 14:55:00 +0900

    Title: 구구단 만들기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250723/구구단 만들기/print-multiplication-table.cpp
+++ 250723/구구단 만들기/print-multiplication-table.cpp
@@ -9,7 +9,7 @@ int main() {
     for(int i = 1; i<=9; i++) {
         for(int k = b; k >= a; k = k-2) {
             cout << k << " * " << i << " = " << k*i;
-            if(k != 2) {
+            if(k != a) {
                 cout << " / ";
             }
         }
