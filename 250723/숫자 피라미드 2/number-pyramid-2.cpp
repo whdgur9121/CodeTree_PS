54d564670f7e7ac3ce764b211d434120985b7959
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-07-23 15:14:20 +0900

    Title: 숫자 피라미드 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250723/숫자 피라미드 2/number-pyramid-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    int count = 1;
+
+    for(int i = 0; i<n; i++) {
+        for(int k = 0; k<=i; k++) {
+            cout << count << " ";
+            count++;
+        }
+        cout << endl;
+    }
+    return 0;
+}
