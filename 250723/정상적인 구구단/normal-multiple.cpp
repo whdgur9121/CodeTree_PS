4d1bfcfb610f5fb9be0d70ba45fa236f41d1da24
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-07-23 14:47:05 +0900

    Title: 정상적인 구구단, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250723/정상적인 구구단/normal-multiple.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >> n;
+
+    for(int i = 1; i<=n; i++) {
+        for(int k = 1; k<=n; k++) {
+            cout << i << " * " << k << " = " << i*k;
+            if(k != n) {
+                cout <<", ";
+            } 
+        }
+        cout << endl;
+    }
+    return 0;
+}
