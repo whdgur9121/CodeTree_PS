8e7d9c63c8ddc7875aa8a1a1202bde89c201a28a
whdgur9121 <163364016+whdgur9121@users.noreply.github.com>
2025-07-23 13:46:17 +0900

    Title: 순서쌍 만들기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250723/순서쌍 만들기/making-order-pair.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    cin >>n;
+
+    for(int i = n; i>0; i--) {
+        for(int k = n; k>0; k--) {
+            cout << "(" << i << "," << k << ") ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
